8c05e228818f7208325b67b886b24b84e2159b49
Vinicius <vinicius.berti33@gmail.com>
2024-03-02 12:19:25 -0300

    test(intro-git): testa subtracao

--- testeCalculadora.py
+++ testeCalculadora.py
@@ -4,6 +4,8 @@ def main():
     a = 2;
     b = 3;
     soma = calculadora.soma(a,b);
-    print(f'{a} + {b} = {soma}')
+    print(f'{a} + {b} = {soma}');
+    subtracao = calculadora.sub(a,b);
+    print(f'{a} - {b} = {subtracao}');
 
 main()
